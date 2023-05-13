c6115daf3cd2aaac20cc8bdb805a85afbb9b5ad0
fdat3 <datp0628@gmail.com>
2023-05-13 23:42:19 +0700

    add twoSum algo

---
+++ twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+    for (i = 0; i < nums.length; i++) {
+        let countPos = nums[i];
+        let countNextPos = nums[i + 1];
+        let posValue = nums[countPos];
+        let nextPosValue = nums[countNextPos];
+        if (posValue[count] + nextPosValue[countNextPos] === target) return [countPos, countNextPos]
+    }
+};
+
+console.log(twoSum([2, 7, 11, 15], 6));
